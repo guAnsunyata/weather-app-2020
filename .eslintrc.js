073dd2b4087a37ffabe4529cc691a3101db7bd63
guansunyata <guansunyata@gmail.com>
2020-04-18 15:49:09 +0800

    chore: update eslint rules & fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,59 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    camelcase: 'off',
+    indent: ['error', 2, { SwitchCase: 1 }],
+    semi: ['error', 'never'],
+    'standard/no-callback-literal': 0,
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+        imports: 'always-multiline',
+        objects: 'always-multiline',
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        tabWidth: 2,
+        arrowParens: 'always',
+        singleQuote: true,
+        trailingComma: 'es5',
+        htmlWhitespaceSensitivity: 'ignore',
+      },
+    ],
+    '@typescript-eslint/member-ordering': 'error',
+    '@typescript-eslint/no-empty-interface': 0,
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
-};
+        jest: true,
+      },
+    },
+  ],
+}
